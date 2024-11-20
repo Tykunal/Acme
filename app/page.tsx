3b13edc296098c16f399351324f993a22bbca19b
Tykunal <kunal2021CSAI065@abesit.edu.in>
2024-11-20 22:41:04 +0530

    Updating with final update for Acme

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Consistency is the key!</strong> Learn with Learnify{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              
             </a>
-            , brought to you by Vercel.
+            
           </p>
           <Link
             href="/login"
